95c9c86c596d83415d03d0a8313d58cc5f814c93
Crowdbotics <team@crowdbotics.com>
2021-08-17 19:07:30 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "proud_hat_29888Identifier",
+  url: "https://proud_hat_29888Identifier.botics.co",
+  api: "https://proud_hat_29888Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
